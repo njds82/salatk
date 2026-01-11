5359c93d3b40feadf008d1aae00f170f54ed4fb1
khaled Al-Khalaylah <ofsorfufhg@gmail.com>
2026-01-11 19:44:17 +0300

    .3

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// Configuration file - DO NOT COMMIT THIS FILE
+// Copy config.example.js and rename to config.js, then add your keys
+
+window.CONFIG = {
+    SUPABASE_URL: 'https://xdvnweeorbuqmrgbtxuu.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhkdm53ZWVvcmJ1cW1yZ2J0eHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0ODgyMjksImV4cCI6MjA4MjA2NDIyOX0.VBK6Be32w89FRJDEu8ypBsSji12CuwxZVX2Jp-5LyXA'
+};
